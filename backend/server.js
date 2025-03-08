1b341086ba77e3900d74fa690537efe32899f179
josh81 <josh.81@rediffmail.com>
2025-03-08 14:49:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 
 const fs = require('fs');
 const https = require('https')
-// const http = require('http')
+const http = require('http')
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -11,12 +11,17 @@ app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 
 
-const key = fs.readFileSync('./certs/key.pem');
-const cert = fs.readFileSync('./certs/cert.pem');
+// const key = fs.readFileSync('./certs/key.pem');
+// const cert = fs.readFileSync('./certs/cert.pem');
 
-const expressServer = https.createServer({key, cert}, app);
+// const expressServer = https.createServer({key, cert}, app);
+const expressServer = http.createServer({}, app);
 const io = socketio(expressServer,{
-    cors: ['https://localhost:3000','https://localhost:3001','https://localhost:3002']
+    cors: ['https://localhost:3000',
+            'https://localhost:3001',
+            'https://localhost:3002',
+            'https://www.clyksnkutz.com'
+            ]
 })
 
 expressServer.listen(9000);
