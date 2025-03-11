f21943405de0f59324fe10844d4297637d5be341
josh81 <josh.81@rediffmail.com>
2025-03-11 11:06:16 +0530

    Update startAudioStream.js
    roll back changes

--- frontend/src/videoComponents/AudioButton/startAudioStream.js
+++ frontend/src/videoComponents/AudioButton/startAudioStream.js
@@ -6,12 +6,10 @@ const startAudioStream = (streams)=>{
     for(const s in streams){ //s is the key
         if(s !== "localStream"){
             //we don't addTracks to the localStream
-            // const curStream = streams[s];
-            const curStream = streams[key];
+            const curStream = streams[s];
             //addTracks to all peerConnecions
             localStream.stream.getAudioTracks().forEach(t=>{
-                curStream.peerConnection.addTrack(t,localStream.stream);
-                // curStream.peerConnection.addTrack(t,streams.localStream.stream);
+                curStream.peerConnection.addTrack(t,streams.localStream.stream);
             })
         }   
     }
