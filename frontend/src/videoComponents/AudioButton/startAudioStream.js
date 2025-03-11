cc96910b64f45041df63dfca8ef2f355ded6223f
josh81 <josh.81@rediffmail.com>
2025-03-11 10:19:14 +0530

    Update startAudioStream.js
    audio button changes(test)

--- frontend/src/videoComponents/AudioButton/startAudioStream.js
+++ frontend/src/videoComponents/AudioButton/startAudioStream.js
@@ -1,16 +1,17 @@
 
 //this functions job is to update all peerConnections (addTracks) and update redux callStatus
-import updateCallStatus from "../../redux-elements/actions/updateCallStatus";
 
 const startAudioStream = (streams)=>{
     const localStream = streams.localStream;
     for(const s in streams){ //s is the key
         if(s !== "localStream"){
             //we don't addTracks to the localStream
-            const curStream = streams[s];
+            // const curStream = streams[s];
+            const curStream = streams[key];
             //addTracks to all peerConnecions
             localStream.stream.getAudioTracks().forEach(t=>{
-                curStream.peerConnection.addTrack(t,streams.localStream.stream);
+                curStream.peerConnection.addTrack(t,localStream.stream);
+                // curStream.peerConnection.addTrack(t,streams.localStream.stream);
             })
         }   
     }
